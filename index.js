22046f77fd0c5511fdde1c8a221b850d3fa35579
Дмитрий <demosne@gmail.com>
2022-02-11 16:45:33 +0300

    handle click

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
 const canvas = document.querySelector("#canv");
 canvas.width = 600;
 canvas.height = 300;
+ctx = canvas.getContext("2d");
 
 const dots = [4, 5, 4, 3, 4, 4.5, 3.2, 7, 5.3, 6.6];
 
-let countOfDots = 10;
-let delayY = canvas.offsetHeight;
-let stepX = canvas.offsetWidth / (countOfDots + 1);
-let diametr = 6;
-let radius = diametr / 2;
-
-if (canvas.getContext) {
-  ctx = canvas.getContext("2d");
+function buildCanvas(arr) {
+  const countOfDots = arr.length;
+  const delayY = canvas.offsetHeight;
+  const stepX = canvas.offsetWidth / (countOfDots + 1);
+  const diametr = 8;
+  const radius = diametr / 2;
 
   ctx.fillStyle = "white";
-  ctx.lineWidth = 0.6;
+  ctx.lineWidth = 0.8;
   ctx.beginPath();
-  ctx.moveTo(stepX, delayY - dots[0] * 10);
+  ctx.moveTo(stepX, delayY - arr[0] * 10);
   ctx.strokeRect(
     stepX - radius,
-    delayY - dots[0] * 10 - radius,
+    delayY - arr[0] * 10 - radius,
     diametr,
     diametr
   );
-  ctx.fillRect(
-    stepX - radius,
-    delayY - dots[0] * 10 - radius,
-    diametr,
-    diametr
-  );
+  ctx.fillRect(stepX - radius, delayY - arr[0] * 10 - radius, diametr, diametr);
 
-  for (let i = 1; i < dots.length; i++) {
-    ctx.lineTo(stepX + stepX * i, delayY - dots[i] * 10);
+  for (let i = 1; i < arr.length; i++) {
+    ctx.lineTo(stepX + stepX * i, delayY - arr[i] * 10);
     ctx.strokeRect(
       stepX + stepX * i - radius,
-      delayY - dots[i] * 10 - radius,
+      delayY - arr[i] * 10 - radius,
       diametr,
       diametr
     );
     ctx.fillRect(
       stepX + stepX * i - radius,
-      delayY - dots[i] * 10 - radius,
+      delayY - arr[i] * 10 - radius,
       diametr,
       diametr
     );
-    ctx.moveTo(stepX + stepX * i, delayY - dots[i] * 10);
+    ctx.moveTo(stepX + stepX * i, delayY - arr[i] * 10);
   }
+
   ctx.fill();
   ctx.stroke();
 }
+
+buildCanvas(dots);
+
+let isDefault = true;
+
+canvas.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  buildCanvas(isDefault ? dots : [4, 5, 4, 3, 4, 4.5]);
+  isDefault = !isDefault;
+});
